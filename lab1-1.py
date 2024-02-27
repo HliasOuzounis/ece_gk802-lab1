16880a6ef414ce47d29f6c8d89fb982f0eb10edb
HliasOuzounis <iliouzoun@outlook.com>
2024-02-27 13:20:38 +0200

    added datetime

--- lab1-1.py
+++ lab1-1.py
@@ -1,4 +1,6 @@
 import requests
+import datetime
+
 
 def main():
     # 1 get URL
@@ -7,24 +9,27 @@ def main():
         url = "www." + url
     if not url.startswith("http"):
         url = "http://" + url
-    
+
     # 2 HTTP request
     with requests.get(url) as response:
         # 3 Print headers
         print(response.headers)
-        
+
         # 4.1 get web server firmware
         server = response.headers.get("Server")
         print(f"Server: {server}" if server else "No server found")
-        
+
         # 4 get cookies
         cookies = response.headers.get("Set-Cookie")
         print("Has cookies" if cookies else "No cookies")
-        
+
         # 5 print cookies
         if cookies:
-            print(cookies)
-        
+            for cookie in response.cookies:
+                print(f"Name: {cookie.name}")
+                if cookie.expires:
+                    print(f"Expires: {datetime.datetime.fromtimestamp(cookie.expires)}")
+                    print(f"Valid for {(datetime.datetime.fromtimestamp(cookie.expires) - datetime.datetime.now()).days} days")
 
 
 if __name__ == "__main__":
